d478f76a6d0a11935b1dd51d2e71137d5f6b1085
a <a@example.org>
2022-02-28 14:59:05 -0600

    add features color select html element

--- prog17_campos_tipo_color.js
+++ prog17_campos_tipo_color.js
@@ -1,7 +1,5 @@
 function ejecutar(){
-	color_fondo=document.f_colores.color_fondo.value;
-	color_texto=document.f_colores.color_texto.value;
-	document.f_colores.res.style.background=color_fondo;
-	document.f_colores.res.style.color=color_texto;
+	document.f_colores.res.style.background=document.f_colores.color_fondo_select.value;
+	document.f_colores.res.style.color=document.f_colores.color_texto_select.value;
 }
 
