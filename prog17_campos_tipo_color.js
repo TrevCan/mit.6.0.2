709001ab0d0011bb588bdfde30675afb31a0d23f
a <a@example.org>
2022-02-28 14:49:31 -0600

    add files

---
+++ prog17_campos_tipo_color.js
@@ -0,0 +1,7 @@
+function ejecutar(){
+	color_fondo=document.f_colores.color_fondo.value;
+	color_texto=document.f_colores.color_texto.value;
+	document.f_colores.res.style.background=color_fondo;
+	document.f_colores.res.style.color=color_texto;
+}
+
